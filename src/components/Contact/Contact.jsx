0009460fbb3c7f35535f84dd64f3817dbbdf8d04
Aniket Tiwari <pt5602238@gmail.com>
2025-06-28 20:57:51 +0530

    contact section is completed

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ import call_icon from '../../assets/call_icon.svg'
 const Contact = () => {
   return (
     <div className="contact">
-        <div className="contact-list">
+        <div className="contact-title">
             <h1>Get in touch</h1>
             <img src={theme_pattern} alt="" />
         </div>
@@ -19,16 +19,26 @@ const Contact = () => {
             <p>I'm currently available to take on new project so feel free to talk</p>
             <div className="contact-details">
                 <div className="contact-detail">
-
+              <img src={mail_icon} alt="" /> <p>pt5602238@gmail.com</p>
                 </div>
              <div className="contact-detail">
-                    
+                    <img src={call_icon} alt="" />  <p>+91 7607107616</p>
                 </div>
           <div className="contact-detail">
-                    
+                    <img src={location_icon} alt="" />  <p>Lucknow, Uttar Pardesh</p>
                 </div>
             </div>
         </div>
+           <form className="contact-right">
+            <label htmlFor="">Your Name</label>
+            <input type="text"  placeholder='enter your name' name='name' />
+            <label htmlFor="">Your Email</label>
+            <input type="email" placeholder='Enter your email' name ='email' />
+            <label htmlFor="">Write your message here</label>
+            <textarea name="message" rows="8" placeholder="Enter your message here"></textarea>
+            <button type='submit' className="contact-submit">Submit now</button>
+
+           </form>
       </div>
     </div>
   )
